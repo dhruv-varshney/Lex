41daffd48f89b572790398b7b1400ce33d92d210
Dhruv Varshney <53403223+dhruv-varshney@users.noreply.github.com>
2021-05-18 23:32:41 +0530

    lex program

---
+++ input2.c
@@ -0,0 +1,31 @@
+//this is the program to count the comment lines 
+
+#include<stdio.h> 
+
+void main() 
+
+{ /*Prolog 
+
+starts here*/  
+
+  int a, b; //declaration statement 
+
+/* 
+
+ 
+
+ 
+
+*/ 
+
+  A= a +b; /* welcome to bit*/ 
+
+       /****************/ 
+
+}// Epilog has encountered 
+
+/******End of the  
+
+Program 
+
+*********/ 
---
+++ output.c
@@ -0,0 +1,33 @@
+
+
+#include<stdio.h> 
+
+void main() 
+
+{ 
+
+  
+
+  int a, b; 
+
+
+
+
+
+
+
+ 
+
+  A= a +b;  
+
+        
+
+}
+
+
+
+
+
+ 
+
+ 
